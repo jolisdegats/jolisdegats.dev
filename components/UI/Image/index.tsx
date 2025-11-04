10185e18142486d910570d67acc70b5635ab0b99
Julie <7164072+jolisdegats@users.noreply.github.com>
2025-11-04 01:44:52 +0100

    fix loading

--- components/UI/Image/index.tsx
+++ components/UI/Image/index.tsx
@@ -1,37 +1,35 @@
-import type { ImageProps, StaticImageData } from 'next/image';
-import NextImage from 'next/image';
-import blurHashes from '@/lib/scripts/blurImages/blurHashes.json';
+import Image from 'next/image';
 
-const Image = ({ src, alt, placeholder, ...props }: ImageProps) => {
-  let blurHash: string | undefined;
+// const Image = ({ src, alt, placeholder, ...props }: ImageProps) => {
+//   let blurHash: string | undefined;
   
-  if (placeholder === 'blur') {
-    const extractFilename = (path: string): string => {
-      const basename = path.split('/').pop() || '';
-      return basename.replace(/\.([a-f0-9]{8})(\.[^.]+)$/, '$2').replace(/^\./, '');
-    };
+//   if (placeholder === 'blur') {
+//     const extractFilename = (path: string): string => {
+//       const basename = path.split('/').pop() || '';
+//       return basename.replace(/\.([a-f0-9]{8})(\.[^.]+)$/, '$2').replace(/^\./, '');
+//     };
     
-    if (typeof src === 'string') {
-      const filename = extractFilename(src);
-      if (filename) {
-        blurHash = blurHashes[filename as keyof typeof blurHashes];
-      }
-    } else if (src && typeof src === 'object' && 'src' in src) {
-      const filename = extractFilename((src as StaticImageData).src);
-      if (filename) {
-        blurHash = blurHashes[filename as keyof typeof blurHashes];
-      }
-    }
-  }
+//     if (typeof src === 'string') {
+//       const filename = extractFilename(src);
+//       if (filename) {
+//         blurHash = blurHashes[filename as keyof typeof blurHashes];
+//       }
+//     } else if (src && typeof src === 'object' && 'src' in src) {
+//       const filename = extractFilename((src as StaticImageData).src);
+//       if (filename) {
+//         blurHash = blurHashes[filename as keyof typeof blurHashes];
+//       }
+//     }
+//   }
   
-  return (
-    <NextImage 
-      src={src} 
-      alt={alt} 
-      {...(blurHash ? { blurDataURL: blurHash } : {})}
-      {...props} 
-    />
-  );
-};
+//   return (
+//     <NextImage 
+//       src={src} 
+//       alt={alt} 
+//       {...(blurHash ? { blurDataURL: blurHash } : {})}
+//       {...props} 
+//     />
+//   );
+// };
 
 export default Image;
